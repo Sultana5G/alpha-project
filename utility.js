3d4d5294202e7943a7ac2e844cb0ba638471d552
Sultana5G <sultanajahan5915@gmail.com>
2024-02-15 10:49:16 +0600

    alpha-2

--- utility.js
+++ utility.js
@@ -4,20 +4,44 @@ function hideElementById(elementId){
 }
 
 function showElementById(elementId) {
-    const element = document.getElementById(elementId);
-    element.classList.remove('hidden')
+    const element = document.getElementById(elementId); ``
+    element.classList.remove('hidden');
 }
 
 function setBackgroundColorById(elementId) {
     const element = document.getElementById(elementId);
-    element.classList.add('bg-orange-400')
+    element.classList.add('bg-orange-400');
+}
+function removeBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
+function getElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const getElementValueText = element.innerText;
+    const value = parseInt(getElementValueText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value) {
+     const element = document.getElementById(elementId);
+     element.innerText = value;
+
+}
+
+function getElementValueTextById(elementId) {
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+
 }
 
 function getARandomAlphabet() {
     // get or create an alphabet array
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('')
-    console.log(alphabets);
+    // console.log(alphabets);
 
     // get a random index 0-25
     const randomNumber = Math.random()*25;
